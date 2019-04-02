ea7f6fa1a610e13f3175778ef6a09bf679b69858
xiao <320777543@qq.com>
2019-04-02 15:50:14 +0800

    schoolshop第一次提交

---
+++ src/main/java/com/my/shcoolshop/model/LocalAuth.java
@@ -0,0 +1,73 @@
+package com.my.shcoolshop.model;
+
+import java.util.Date;
+
+public class LocalAuth {
+	//本地帐号ID
+	private Long localAuthId;
+	private String userName;
+	private String password;
+	//用户ID
+	private Long userId;
+	private Date createTime;
+	private Date lastEditTime;
+	//用户信息
+	private PersonInfo personInfo;
+
+	public Long getLocalAuthId() {
+		return localAuthId;
+	}
+
+	public void setLocalAuthId(Long localAuthId) {
+		this.localAuthId = localAuthId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getLastEditTime() {
+		return lastEditTime;
+	}
+
+	public void setLastEditTime(Date lastEditTime) {
+		this.lastEditTime = lastEditTime;
+	}
+
+	public PersonInfo getPersonInfo() {
+		return personInfo;
+	}
+
+	public void setPersonInfo(PersonInfo personInfo) {
+		this.personInfo = personInfo;
+	}
+
+}
---
+++ src/main/java/com/my/shcoolshop/model/WechatAuth.java
@@ -0,0 +1,55 @@
+package com.my.shcoolshop.model;
+
+import java.util.Date;
+
+public class WechatAuth {
+	//微信用户ID
+	private Long wechatAuthId;
+	//用户id
+	private Long userId;
+	//微信openId
+	private String openId;
+	private Date createTime;
+	private PersonInfo personInfo;
+
+	public Long getWechatAuthId() {
+		return wechatAuthId;
+	}
+
+	public void setWechatAuthId(Long wechatAuthId) {
+		this.wechatAuthId = wechatAuthId;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getOpenId() {
+		return openId;
+	}
+
+	public void setOpenId(String openId) {
+		this.openId = openId;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public PersonInfo getPersonInfo() {
+		return personInfo;
+	}
+
+	public void setPersonInfo(PersonInfo personInfo) {
+		this.personInfo = personInfo;
+	}
+
+}
