9ead8bf10a7dcf3165e5fe3dbc60860a87c2dd79
xiao <320777543@qq.com>
2019-04-08 15:04:56 +0800

    新增图片处理

---
+++ src/main/java/com/my/schoolshop/util/FileUtil.java
@@ -0,0 +1,76 @@
+package com.my.schoolshop.util;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+public class FileUtil {
+    private static String seperator = System.getProperty("file.seperator");//获取系统分割符号
+    private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+    private static final Random r = new Random();
+
+    public static String getImgBasePath(){
+        //获取系统名称
+        String os = System.getProperty("os.name");
+        String basePath = "";
+        if(os.toLowerCase().startsWith("win")){
+            basePath = "D:/projectdev/img";
+        }else {
+            basePath = "/home/xiangzepro";
+        }
+
+        basePath = basePath.replace("/",seperator);//替换路径分割符号
+        return basePath;
+    }
+
+    //获取不同类别图片的系统存放路径
+    public static String getHeadLineImagePath(){
+        String headLineImagePath = "/upload/images/item/headline/";
+        headLineImagePath = headLineImagePath.replace("/",seperator);
+        return headLineImagePath;
+    }
+
+    public static String getShopCategoryImagePath(){
+        String shopCategoryImagePath = "/upload/images/item/shopcategory/";
+        shopCategoryImagePath = shopCategoryImagePath.replace("/",seperator);
+        return shopCategoryImagePath;
+    }
+
+    public static String getPersonInfoImagePath(){
+        String personInfoImagePath = "/upload/images/item/personinfo/";
+        personInfoImagePath = personInfoImagePath.replace("/",seperator);
+        return personInfoImagePath;
+    }
+    public static String getShopImagePath(long shopId) {
+        StringBuilder shopImagePathBuilder = new StringBuilder();
+        shopImagePathBuilder.append("/upload/images/item/shop/");
+        shopImagePathBuilder.append(shopId);
+        shopImagePathBuilder.append("/");
+        String shopImagePath = shopImagePathBuilder.toString().replace("/",seperator);
+        return shopImagePath;
+    }
+
+    public static String getRandomFileName() {
+        // 生成随机文件名：当前年月日时分秒+五位随机数（为了在实际项目中防止文件同名而进行的处理）
+        int rannum = (int) (r.nextDouble() * (99999 - 10000 + 1)) + 10000; // 获取随机数
+        String nowTimeStr = simpleDateFormat.format(new Date()); // 当前时间
+        return nowTimeStr + rannum;
+    }
+
+    public static void deleteFile(String storePath) {
+        File file = new File(getImgBasePath() + storePath);
+        if (file.exists()) {
+            if (file.isDirectory()) {
+                File files[] = file.listFiles();
+                for (int i = 0; i < files.length; i++) {
+                    files[i].delete();
+                }
+            }
+            file.delete();
+        }
+    }
+
+
+
+}
---
+++ src/main/java/com/my/schoolshop/util/ImageUtil.java
@@ -0,0 +1,78 @@
+package com.my.schoolshop.util;
+
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageUtil {
+
+    public static String generateThumbnail(CommonsMultipartFile thumbnail, String targetAddr) {//CommonsMultipartFile 缩略图文件
+        String realFileName = FileUtil.getRandomFileName();//获取随机文件名称
+        String extension = getFileExtension(thumbnail);//获取文件名后缀
+        makeDirPath(targetAddr);//创建目录
+        String relativeAddr = targetAddr + realFileName + extension;
+        File dest = new File(FileUtil.getImgBasePath() + relativeAddr);
+        try {
+            Thumbnails.of(thumbnail.getInputStream()).size(200, 200).outputQuality(0.25f).toFile(dest);
+        } catch (IOException e) {
+            throw new RuntimeException("创建缩略图失败：" + e.toString());
+        }
+        return relativeAddr;
+    }
+
+    public static String generateNormalImg(CommonsMultipartFile thumbnail, String targetAddr) {
+        String realFileName = FileUtil.getRandomFileName();
+        String extension = getFileExtension(thumbnail);
+        makeDirPath(targetAddr);
+        String relativeAddr = targetAddr + realFileName + extension;
+        File dest = new File(FileUtil.getImgBasePath() + relativeAddr);
+        try {
+            Thumbnails.of(thumbnail.getInputStream()).size(337, 640).outputQuality(0.5f).toFile(dest);//将上传的图片按照一定比例缩放
+        } catch (IOException e) {
+            throw new RuntimeException("创建缩略图失败：" + e.toString());
+        }
+        return relativeAddr;
+    }
+
+    //批量上传图片处理
+    public static List<String> generateNormalImgs(List<CommonsMultipartFile> imgs, String targetAddr) {
+        int count = 0;
+        List<String> relativeAddrList = new ArrayList<String>();
+        if (imgs != null && imgs.size() > 0) {
+            makeDirPath(targetAddr);
+            for (CommonsMultipartFile img : imgs) {
+                String realFileName = FileUtil.getRandomFileName();
+                String extension = getFileExtension(img);
+                String relativeAddr = targetAddr + realFileName + count + extension;
+                File dest = new File(FileUtil.getImgBasePath() + relativeAddr);
+                count++;
+                try {
+                    Thumbnails.of(img.getInputStream()).size(600, 300).outputQuality(0.5f).toFile(dest);
+                } catch (IOException e) {
+                    throw new RuntimeException("创建图片失败：" + e.toString());
+                }
+                relativeAddrList.add(relativeAddr);
+            }
+        }
+        return relativeAddrList;
+    }
+
+    private static void makeDirPath(String targetAddr) {
+        String realFileParentPath = FileUtil.getImgBasePath() + targetAddr;
+        File dirPath = new File(realFileParentPath);
+        if (!dirPath.exists()) {
+            dirPath.mkdirs();
+        }
+    }
+
+    //截取上传文件的后缀
+    private static String getFileExtension(CommonsMultipartFile cFile) {
+        String originalFileName = cFile.getOriginalFilename();
+        return originalFileName.substring(originalFileName.lastIndexOf("."));
+    }
+
+}
