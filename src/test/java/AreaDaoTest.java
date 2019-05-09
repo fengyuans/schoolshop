d90c61ffe254670b06338a1310b1d3638edb753d
xiao <320777543@qq.com>
2019-05-09 09:42:13 +0800

    新增

---
+++ src/test/java/AreaDaoTest.java
@@ -0,0 +1,22 @@
+import com.my.schoolshop.dao.AreaDao;
+import com.my.schoolshop.model.Area;
+import com.my.schoolshop.service.AreaService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AreaDaoTest extends BaseTest {
+    @Autowired
+    private AreaDao areaDao;
+
+    @Autowired
+    private AreaService service;
+
+
+    @Test
+    public void queryAllTest(){
+        List<Area> list = service.getAreaList();
+        System.out.println(list);
+    }
+}
